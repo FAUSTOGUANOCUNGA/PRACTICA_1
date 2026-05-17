1f0dcc606e10a34e1c26859918e07d3379d254df
FAUSTOGUANOCUNGA <fausto.guanocunga@ister.edu.ec>
2026-05-16 22:46:53 -0500

    primer commint

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+var1 = 5.5
+print(var1)
+
+var2 = int(5.5)
+print(var2)
+
+nombre = 'Javier'
+apellido = 'Guanocunga'
+edad = 37
+print(f"{nombre}, {apellido}")
+print(f"{nombre} {apellido} tiene {edad}años")
